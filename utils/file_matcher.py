ff2b23ef4e6d574c00ffa19a03da98e6dfcd97b8
tianyaqu <tianyaqu@qq.com>
2015-07-10 17:37:18 +0800

    add utils

---
+++ utils/file_matcher.py
@@ -0,0 +1,17 @@
+import os
+import fnmatch
+
+def file_filter(path,filters):
+    try:
+        files = os.listdir(path)
+        for filter in filters:
+            for f in fnmatch.filter(files, "*.%s" % filter):
+                p = os.path.join(path, f)
+                yield (p, filter)
+    except:
+        print 'www'
+        
+if __name__ == '__main__':
+    x = file_filter('c:\\src',['mp3'])
+    for k in x:
+        print k
